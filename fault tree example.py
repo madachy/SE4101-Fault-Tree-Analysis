02d79930da0ff46e0624cbf243e9de50a367fbe0
Ray Madachy <madachy@gmail.com>
2022-05-10 15:16:50 -0700

    Create fault tree example.py

---
+++ fault tree example.py
@@ -0,0 +1,12 @@
+import pyml
+
+lamp_circuit_fault_tree = [("Missing Indication", "and", ["Resistor Fails", "Capacitor Fails", "Both Lamps Out"]),
+           ('Both Lamps Out', 'or', ['Lamp 1 Fails', 'Lamp 2 Fails', 'Lamp 3 Fails']),
+           ('Resistor Fails', 'basic', []),
+           ('Capacitor Fails', 'basic', []),
+           ('Lamp 1 Fails', 'BASIC', []),
+           ('Lamp 2 Fails', 'basic', []),
+           ('Lamp 3 Fails', 'basic', []),
+                          ]
+
+pyml.fault_tree_diagram(lamp_circuit_fault_tree, filename="lamp_circuit_fault_tree", format='png')
